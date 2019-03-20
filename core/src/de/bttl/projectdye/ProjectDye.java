b915a1fa6b30df586232610871b92f5f10974591
Battleju <godanbo@googlemail.com>
2019-03-20 13:18:38 +0100

    .

--- core/src/de/bttl/projectdye/ProjectDye.java
+++ core/src/de/bttl/projectdye/ProjectDye.java
@@ -11,6 +11,7 @@ import de.bttl.projectdye.states.MenuState;
 
 public class ProjectDye extends ApplicationAdapter {
 
+	//skyr
 	public static final int WIDTH = 1280;
 	public static final int HEIGHT = 720;
 	public static final String TITLE = "ProjectDye";
